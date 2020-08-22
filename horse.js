4802b721b460d181884472a358e8a8ce6dba462d
Huynh Nguyen Toan <huynhnguyentoan@gmail.com>
2020-08-22 17:04:52 +0700

    Horse run method

--- horse.js
+++ horse.js
@@ -3,6 +3,9 @@ function Horse() {
 
 Horse.prototype.jump = function () {
 	console.log('Jumped...');
+
+Horse.prototype.run = function() {
+    console.log('Running');
 }
 
 module.exports = Horse;
